227c4caf382bb89c7c1a056a67bfaed72bafc95e
crcarpent <crcarpent@gmail.com>
2023-12-05 08:34:33 -0700

    i am confused.

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('hellowrold');
+
+const names = ['foo', 'bar', 'bazz', 'quq', 'fizz', 'buzz']
+function generateRandom() {
+    return {
+
+    };
+}
+
+const products = [];
+products.push(generateRandom());
+products.push(generateRandom());
+console.log(products)
